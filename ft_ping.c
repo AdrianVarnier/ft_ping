2397e3952daa06823745f4b3ba34f0ba76efb4a0
Adrian VARNIER <avarnier@paul-f3Br7s2.clusters.42paris.fr>
2024-12-19 18:00:15 +0100

    add time display

--- ft_ping.c
+++ ft_ping.c
@@ -13,6 +13,35 @@ unsigned short checksum(unsigned short* ptr, int bytes)
     return (~sum);
 }
 
+void print_icmp_reply(char *reply, int bytes_received, struct sockaddr_in *reply_addr, struct timespec *send_time) {
+    // Parse IP header
+    struct iphdr *ip_hdr = (struct iphdr *)reply;
+    int ip_header_len = ip_hdr->ihl * 4;
+
+    // Parse ICMP header
+    struct icmp *icmp_hdr = (struct icmp *)(reply + ip_header_len);
+
+    if (icmp_hdr->icmp_type != ICMP_ECHOREPLY) {
+        fprintf(stderr, "Non-echo reply received: type=%d code=%d\n", icmp_hdr->icmp_type, icmp_hdr->icmp_code);
+        return;
+    }
+
+    // Calculate RTT
+    struct timespec receive_time;
+    clock_gettime(CLOCK_MONOTONIC, &receive_time);
+
+    double rtt = (receive_time.tv_sec - send_time->tv_sec) * 1000.0; // seconds to ms
+    rtt += (receive_time.tv_nsec - send_time->tv_nsec) / 1000000.0;  // nanoseconds to ms
+
+    // Print output
+    printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.2f ms\n",
+           bytes_received - ip_header_len,
+           inet_ntoa(reply_addr->sin_addr),
+           icmp_hdr->icmp_seq,
+           ip_hdr->ttl,
+           rtt);
+}
+
 void    ft_ping(const char* target, int verbose)
 {
     // check target
@@ -44,6 +73,8 @@ void    ft_ping(const char* target, int verbose)
     icmp_packet->icmp_cksum = checksum((unsigned short *)packet, sizeof(packet));
 
     // send packet
+    struct timespec send_time;
+    clock_gettime(CLOCK_MONOTONIC, &send_time);
     if (sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr *)&addr, sizeof(addr)) <= 0)
     {
         perror("sendto");
@@ -55,15 +86,14 @@ void    ft_ping(const char* target, int verbose)
     char reply[1024];
     struct sockaddr_in reply_addr;
     socklen_t addr_len = sizeof(reply_addr);
-    if (recvfrom(sockfd, reply, sizeof(reply), 0, (struct sockaddr *) &reply_addr, &addr_len) <= 0)
+    int bytes_read = recvfrom(sockfd, reply, sizeof(reply), 0, (struct sockaddr *) &reply_addr, &addr_len);
+    if (bytes_read <= 0)
     {
         perror("recvfrom");
     }
     else
     {
-        if (verbose)
-            printf("verbose\n");
-        printf("Received ICMP reply from %s\n", inet_ntoa(reply_addr.sin_addr));
+        print_icmp_reply(reply, bytes_received, &reply_addr, &send_time);
     }
 
     close(sockfd);
